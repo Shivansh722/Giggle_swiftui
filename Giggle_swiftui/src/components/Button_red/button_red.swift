59ce46d7d95843948623361fe7a11fbdf626ec13
Shivansh722 <shivanshcha722@gmail.com>
2024-11-02 14:03:51 +0530

    feat: add custom red button

--- Giggle_swiftui/src/components/Button_red/button_red.swift
+++ Giggle_swiftui/src/components/Button_red/button_red.swift
@@ -7,3 +7,26 @@
 
 import SwiftUI
 
+struct CustomButton: View {
+    var title: String
+    var backgroundColor: Color
+    var action: () -> Void //
+    var width: CGFloat = .infinity
+    var height: CGFloat = .infinity
+  
+    
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .foregroundColor(.white)
+                .fontWeight(.bold)
+                .padding()
+                .frame(width: width, height: height)//use width and height param in respec files
+                .background(Theme.primaryColor)
+                .cornerRadius(10)
+            
+            
+        }
+        .padding(.horizontal, 30)
+    }
+}
