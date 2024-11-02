a77ed041295180bb77c559011ada5683c7b83913
Shivansh722 <shivanshcha722@gmail.com>
2024-11-02 13:24:55 +0530

    feat: add components folder
    button_red and textfield

---
+++ Giggle_swiftui/src/components/Button_red/button_red.swift
@@ -0,0 +1,7 @@
+//
+//  button_red.swift
+//  Giggle_swiftui
+//
+//  Created by user@91 on 02/11/24.
+//
+
---
+++ Giggle_swiftui/src/components/TextField/textfield.swift
@@ -0,0 +1,7 @@
+//
+//  textfield.swift
+//  Giggle_swiftui
+//
+//  Created by user@91 on 02/11/24.
+//
+
