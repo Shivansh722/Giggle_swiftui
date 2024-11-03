b21ec26131929f0335ff1b440dc3f1d2bbcaee74
Shivansh722 <shivanshcha722@gmail.com>
2024-11-03 18:34:27 +0530

    feat: add user details file n folder

---
+++ Giggle_swiftui/src/features/Authentication/user-detail/models/view/user.swift
@@ -0,0 +1,12 @@
+//
+//  user.swift
+//  Giggle_swiftui
+//
+//  Created by user@91 on 03/11/24.
+//
+
+import SwiftUI
+
+struct userView {
+    
+}
---
+++ Giggle_swiftui/src/features/user-detail/view/user-detail.swift
@@ -0,0 +1,45 @@
+//
+//  user-detail.swift
+//  Giggle_swiftui
+//
+//  Created by user@91 on 03/11/24.
+//
+
+import SwiftUI
+
+struct user_detail: View {
+    var body: some View {
+        
+        ZStack {
+            Theme.backgroundColor
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                
+                HStack(alignment: .top) {
+                    VStack(alignment: .leading, spacing: 5) {
+                        HStack {
+                            Text("User")
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .foregroundColor(Theme.primaryColor)
+                            
+                            Text("Details")
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .foregroundColor(Theme.onPrimaryColor)
+                        }
+                    }
+                    .padding(.leading, 30)
+                    
+                    Spacer()
+                }
+                .padding(.top, 30)
+            }
+        }
+    }
+}
+
+#Preview {
+    user_detail()
+}
