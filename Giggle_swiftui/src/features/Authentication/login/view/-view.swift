9b3d4dd46375b8b65d0fdfcf0192bda5958d8425
Shivansh722 <shivanshcha722@gmail.com>
2024-11-05 15:35:42 +0530

    feat: add login screen for without fln users

--- Giggle_swiftui/src/features/Authentication/login/view/-view.swift
+++ Giggle_swiftui/src/features/Authentication/login/view/-view.swift
@@ -1,7 +1,116 @@
-//
-//  login.swift
-//  Giggle_swiftui
-//
-//  Created by user@91 on 05/11/24.
-//
+import SwiftUI
 
+struct LoginSimpleView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Theme.backgroundColor
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack() {
+                    // Welcome Back Text
+                    HStack(alignment: .top) {
+                        VStack(alignment: .leading, spacing: 5) {
+                            HStack {
+                                Text("Welcome")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Theme.primaryColor)
+                                
+                                Text("Back!")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Theme.onPrimaryColor)
+                            }
+                            // Plane Image below Welcome Back
+                            Image("plane")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 400, height: 450) // Adjust width and height as needed
+                                .padding(.top, -140) // Adjust spacing as desired
+                           
+                        }
+                        .padding(.leading, 30)
+                        
+                        Spacer()
+                    }
+                    .padding(.top, 20)
+                    
+                    // Email and Password Fields
+                    CustomTextField(placeholder: "Email", isSecure: false, text: $email, icon: "envelope")
+                        .padding(.bottom, 12)
+                        
+                    
+                    CustomTextField(placeholder: "Password", isSecure: true, text: $password, icon: "lock")
+                        .padding(.bottom, 20)
+                    
+                    // Sign In Button
+                    CustomButton(
+                        title: "SIGN IN",
+                        backgroundColor: Theme.primaryColor,
+                        action: {
+                            // Sign in action
+                        },
+                        width: 340,
+                        height: 50,
+                        cornerRadius: 6
+                    )
+                    .padding(.top, 20)
+
+                    // Divider with OR text
+                    HStack {
+                        Divider()
+                            .frame(width: 110, height: 1)
+                            .background(Color.gray)
+
+                        Text("OR")
+                            .foregroundColor(Color.gray)
+
+                        Divider()
+                            .frame(width: 110, height: 1)
+                            .background(Color.gray)
+                    }
+                    .padding(.vertical, 20)
+
+                    // Social Login Icons
+                    HStack(spacing: 40) {
+                        Image("google-logo")
+                            .resizable()
+                            .frame(width: 81, height: 75)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                        
+                        Image("apple-logo")
+                            .resizable()
+                            .frame(width: 81, height: 75)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                    .padding(.top, 20)
+
+                    Spacer()
+
+                    // Bottom registration link
+                    HStack {
+                        Text("Not a member?")
+                            .foregroundColor(Theme.onPrimaryColor)
+                        
+                        NavigationLink(destination: RegisterView()) {
+                            Text("Register Now")
+                                .foregroundColor(Theme.primaryColor)
+                                .fontWeight(.bold)
+                        }
+                    }
+                    .padding(.bottom, 30)
+                }
+                .padding(.horizontal, 20)
+            }
+        }
+        .navigationBarHidden(true)
+    }
+}
+
+#Preview {
+    LoginSimpleView()
+}
