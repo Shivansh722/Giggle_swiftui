853b1a0b41dfa082875da9a7e17d76e77a0b92bf
Shivansh722 <shivanshcha722@gmail.com>
2024-11-12 11:23:29 +0530

    feat: add title and progress bar

--- Giggle_swiftui/src/features/formfield/location/location-view.swift
+++ Giggle_swiftui/src/features/formfield/location/location-view.swift
@@ -11,7 +11,42 @@ struct locationView: View {
     
     var body: some View {
         
-        Text("Hello, World!")
+        GeometryReader { geometry in
+            
+            
+            ZStack {
+                Theme.backgroundColor
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    
+                    HStack(alignment: .top) {
+                        VStack(alignment: .leading, spacing: 5) {
+                            HStack {
+                                Text("Location")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Theme.primaryColor)
+                                Text("Details")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Theme.onPrimaryColor)
+                            }
+                            .padding(.leading, geometry.size.width * 0.08)
+                        }
+                        Spacer()
+                    }
+                    .padding(.top, geometry.size.height * 0.02)
+                    
+                    ProgressView(value: 20, total: 100)
+                        .accentColor(Theme.primaryColor)
+                        .padding(.horizontal, geometry.size.width * 0.08)
+                        .padding(.bottom, 20)
+                    
+                    
+                }
+                
+            }
+        }
     }
 }
 
